8da4e1c17217ec0296c999fb2714ce13834fb2b7
Ricardo Medina <rcrdmedina6@gmail.com>
2018-03-11 16:24:51 -0430

    Creando un servidor de desarrollo con webpack y agregando soporte ecmascript

---
+++ babel-loader/index.js
@@ -0,0 +1,6 @@
+
+import './estilos.css'
+import message from './message.js'
+// console.log('hola mundo desde webpack');
+document.write(message.firstMessage)
+console.log('Hola Mundo!, usando modulo')
